447fc01bd33222042f4c8a1fbf6be9e1f3aefb23
Agustin C <33077305+agusc01@users.noreply.github.com>
2026-01-25 12:22:16 -0300

    Fix contact.php, Explain how to use it

--- pages/nitsuga/views/contact.php
+++ pages/nitsuga/views/contact.php
@@ -1,14 +1,18 @@
+<?php
+    require_once 'helpers/url.php';
+    // README: if without Script , you have to put BEFORE the HTML
+    // URL::redirectTo('cupoftea');
+?>
 <?php include_once 'pages/nitsuga/views/components/head.php'; ?>
 <!-- More links or scripts -->
 </head>
 <body>
 <?php
-    require_once 'helpers/url.php';
 
     echo "Contact. Nitsuga<hr>";
     include_once 'pages/nitsuga/views/components/navbar.php';
 
-    // URL::redirectTo('cupoftea');
+    // README: if with Script , you have to put AFTER the HTML
     URL::redirectToScript('cupoftea');
 ?>
 </body>
